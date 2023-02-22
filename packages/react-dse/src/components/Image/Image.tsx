7d5bab936b1493bb8e0c28976b91dcc598bd49b7
VoN <sudo.von.contact@gmail.com>
2023-02-22 12:23:20 -0600

    feat: :sparkles: add component and story

---
+++ packages/react-dse/src/components/Image/Image.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+
+interface IImageProps {
+    src: string;
+    alt?: string;
+    width?: number;
+    height?: number;
+    fit?: keyof typeof imageVariant.fit,
+}
+
+const imageVariant = {
+    fit: {
+        contain: 'object-contain',
+        cover: 'object-cover',
+        fill: 'object-fill',
+        none: 'object-none',
+        scale: 'object-scale-down',
+    }
+};
+
+const Image: FC<IImageProps> = ({
+    src,
+    alt,
+    width,
+    height,
+    fit = 'none',
+}) => {
+    const className = `${imageVariant.fit[fit]}`
+    return (
+        <img src={src} alt={alt} className={className} height={height} width={width} />
+    );
+};
+
+export default Image;
