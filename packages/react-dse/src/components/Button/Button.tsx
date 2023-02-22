6afd8be879e5e81acadaa3c59ce63c2545f50913
VoN <sudo.von.contact@gmail.com>
2023-02-22 10:21:04 -0600

    feat: :sparkles: add type

--- packages/react-dse/src/components/Button/Button.tsx
+++ packages/react-dse/src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ interface IButtonProps {
     variety?: keyof typeof buttonVariety,
     type?: 'submit' | 'button' | 'reset',
     children: ReactNode,
+    onClick?: VoidFunction,
 }
 
 const buttonVariety = {
@@ -18,12 +19,14 @@ const Button: FC<IButtonProps> = ({
     type = 'submit',
     variety = 'primary',
     children,
+    onClick,
 }) => {
     const className = `${buttonVariety[variety]} text-white font-bold py-2 px-4 rounded-full`
     return (
         <button
             type={type}
             className={className}
+            onClick={onClick}
         >
             {children}
         </button>
