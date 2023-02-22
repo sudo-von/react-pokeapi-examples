b9fa37aecd280ec941d2f8b1f82166373ca1a05f
VoN <sudo.von.contact@gmail.com>
2023-02-22 12:43:29 -0600

    feat: :sparkles: add missing exports, remove unnecesary files

--- packages/react-dse/src/index.tsx
+++ packages/react-dse/src/index.tsx
@@ -2,6 +2,8 @@ import './index.css';
 
 export { default as Badge } from './components/Badge/Badge';
 export { default as Button } from './components/Button/Button';
+export { default as Card } from './components/Card/Card';
+export { default as Image } from './components/Image/Image';
 export { default as Input } from './components/Input/Input';
 export { default as Search } from './components/Search/Search';
 export { default as Typography } from './components/Typography/Typography';
