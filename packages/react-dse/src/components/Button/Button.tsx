3e589a5e0ce7f40c9b4e6ffa49931f2979b19d1d
VoN <sudo.von.contact@gmail.com>
2023-02-22 10:16:04 -0600

    feat: :sparkles: add component

---
+++ packages/react-dse/src/components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import { FC, ReactNode } from 'react';
+
+interface IButtonProps {
+    variety?: keyof typeof buttonVariety,
+    type?: 'submit' | 'button' | 'reset',
+    children: ReactNode,
+}
+
+const buttonVariety = {
+    primary: 'bg-blue-500 hover:bg-blue-700',
+    secondary: 'bg-green-500 hover:bg-green-700',
+    danger: 'bg-rose-500 hover:bg-rose-700',
+    warning: 'bg-amber-500 hover:bg-amber-700',
+    info: 'bg-sky-500 hover:bg-sky-700',
+};
+
+const Button: FC<IButtonProps> = ({
+    type = 'submit',
+    variety = 'primary',
+    children,
+}) => {
+    const className = `${buttonVariety[variety]} text-white font-bold py-2 px-4 rounded-full`
+    return (
+        <button
+            type={type}
+            className={className}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
