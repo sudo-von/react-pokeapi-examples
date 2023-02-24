c270e732c447d4b15f4c892844d8264a52254602
VoN <sudo.von.contact@gmail.com>
2023-02-24 03:05:38 -0600

    feat: :sparkles: add webpack config file

---
+++ packages/dse/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: "ts-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    },
+};
