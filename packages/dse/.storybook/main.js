e157908297e6f21a15fc8b215f694a0c2654097a
VoN <sudo.von.contact@gmail.com>
2023-02-24 03:50:52 -0600

    feat: :sparkles: add postcss support

--- packages/dse/.storybook/main.js
+++ packages/dse/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -11,5 +13,18 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  "webpackFinal": async (config) => {
+    config.module.rules.push({
+      "test": /\.css$/i,
+      "use": [
+        {
+          "loader": "postcss-loader",
+          "options": { implementation: require.resolve("postcss") },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
   }
 }
