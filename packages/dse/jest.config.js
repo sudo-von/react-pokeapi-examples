8cfed2ca950742df1ba26505d8746bce2775c16c
VoN <sudo.von.contact@gmail.com>
2023-02-28 15:54:50 -0600

    build: :hammer: update jest config

--- packages/dse/jest.config.js
+++ packages/dse/jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+        "!<rootDir>/node_modules/",
+        "!<rootDir>/src/**/*.d.{js,jsx,ts,tsx}",
+        "!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}"
+    ],
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
